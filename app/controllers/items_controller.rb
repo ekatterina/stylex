caec095427de498056a8d862056d5a10dedb6d24
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-08-17 20:42:16 +0200

    actions in items_controller new|create|show|item_params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,21 @@
 class ItemsController < ApplicationController
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to item_path(@item)
   end
 
   def show
+    @items = Item.all
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :this)
   end
 end
