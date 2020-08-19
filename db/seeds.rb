6fea3e21ab09c16c081264e211e7fd175665f0d4
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-08-19 15:39:18 +0200

    I lend done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,27 +7,27 @@ require 'faker'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-3.times do
- user = User.new(
-  email: Faker::Internet.email,
-  password: "123456"
-  )
-  user.save!
-end
+# 3.times do
+#  user = User.new(
+#   email: Faker::Internet.email,
+#   password: "123456"
+#   )
+#   user.save!
+# end
 
-10.times do
-  item = Item.new(
-    title:    Faker::Company.name,
-    pickup_location: "#{Faker::Address.street_address}, #{Faker::Address.city}",
-    price: rand(100..500),
-    color: Faker::Color.color_name,
-    category: ["T-shirt", "Skirt", "Trousers", "Jeans"].sample,
-    description: Faker::Lorem.sentence(word_count: 15),
-    user_id: rand(1..3),
-    size: "XL"
-  )
-  item.save!
+# 10.times do
+#   item = Item.new(
+#     title:    Faker::Company.name,
+#     pickup_location: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+#     price: rand(100..500),
+#     color: Faker::Color.color_name,
+#     category: ["T-shirt", "Skirt", "Trousers", "Jeans"].sample,
+#     description: Faker::Lorem.sentence(word_count: 15),
+#     user_id: rand(1..3),
+#     size: "XL"
+#   )
+#   item.save!
 
-  file = URI.open('https://www1.assets-gap.com/webcontent/0018/172/553/cn18172553.jpg')
-  item.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
-end
+#   file = URI.open('https://www1.assets-gap.com/webcontent/0018/172/553/cn18172553.jpg')
+#   item.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
+# end
