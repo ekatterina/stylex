44758c414413aded30c7527a278bd75bec78a0a0
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-08-19 15:00:59 +0200

    I lend done

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class DashboardController < ApplicationController
   def show
     @items = Item.all
+    @bookings = Booking.all
     authorize @items
+    authorize @bookings
+    @user = current_user
   end
 end
