4f2894b5201e4b8e565b95ced4a3028e6c1eef0a
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-08-20 16:44:53 +0200

    dashboard functionallity fixed

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,22 @@
 class DashboardController < ApplicationController
   def show
-    @items = Item.all
-    @bookings = Booking.all
-    authorize @items
-    authorize @bookings
     @user = current_user
+    @items = Item.where(user_id: current_user.id)
+
+    bookings = Booking.where(user_id: current_user.id)
+    @borrowed_items = bookings.map do |booking|
+      booking.item
+    end
+    authorize @items
+    # authorize @borrowed_items
+
+    # @user = current_user
+    # @booking = Booking.where(user_id: current_user.id)
+    # # thing = @booking.class_variable_get(:user_id)
+
+    # # item_book = Booking.where(user_id: current_user.id)
+    # # @bookings = Item.where(item_id: item_book.class_variable_get(:item_id))
+    # authorize @items
+    # authorize @booking
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :items
   has_many :bookings
+  has_many :items, through: :bookings
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
