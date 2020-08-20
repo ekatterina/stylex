c2049f19f6d642eb523c178ff9e6d6e689d04fd0
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-08-20 12:52:54 +0200

    seeds with working pictures

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
-  # belongs_to :variable
 
-  has_one_attached :photo
   geocoded_by :pickup_location
   after_validation :geocode, if: :will_save_change_to_pickup_location?
 
   has_many_attached :individual_pieces
   has_one_attached :style_photo
-
 
   validates :title, presence: true
   validates :style_photo, presence: true
